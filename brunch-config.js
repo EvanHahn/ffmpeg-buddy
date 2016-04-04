9453519e7c3cc9aac196f77e7e4314b49b5c1515
Evan Hahn <me@evanhahn.com>
2016-04-03 21:32:40 -0700

    brunch new

---
+++ brunch-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  // See http://brunch.io for documentation.
+  files: {
+    javascripts: {joinTo: 'app.js'},
+    stylesheets: {joinTo: 'app.css'},
+    templates: {joinTo: 'app.js'}
+  }
+}
